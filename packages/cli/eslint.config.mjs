d92be84f6637eeec2f68a24abedb64ae4eb28f70
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2025-01-12 20:27:15 -0300

    Relayer: Remove react lint rules (#5)

--- packages/cli/eslint.config.mjs
+++ packages/cli/eslint.config.mjs
@@ -3,7 +3,7 @@ import eslintConfigMimic from 'eslint-config-mimic'
 // Remove react rules
 delete eslintConfigMimic[1].plugins.react
 Object.keys(eslintConfigMimic[1].rules)
-  .filter(rule => rule.includes("react"))
+  .filter(rule => rule.includes('react'))
   .forEach(rule => delete eslintConfigMimic[1].rules[rule])
 
 export default eslintConfigMimic
