128d335ca35d36174641f71e02dc17b91fb9e75f
PedroAraoz <paraoz22@gmail.com>
2025-01-13 16:40:14 -0300

    relayer: add manifest inputs

--- packages/example/src/task.ts
+++ packages/example/src/task.ts
@@ -1,11 +1,12 @@
 import { environment } from '@mimicprotocol/lib-ts'
+import { input } from './types'
 
 export default function main(): void {
   const firstNumber = environment.getValue()
-  const firstIntent = firstNumber * 2
+  const firstIntent = firstNumber * input.firstStaticNumber
   environment.createIntent(firstIntent)
 
   const secondNumber = environment.getValue()
-  const secondIntent = secondNumber * 3
+  const secondIntent = secondNumber * input.secondStaticNumber
   environment.createIntent(secondIntent)
 }
--- packages/relayer/src/index.ts
+++ packages/relayer/src/index.ts
@@ -6,11 +6,15 @@ import Environment from './environment'
 export async function executeTask(opts: { dir: string }) {
   const wasmPath = path.join(opts.dir, 'task.wasm')
   const inputsPath = path.join(opts.dir, 'inputs.json')
+  // const manifestPath = path.join(opts.dir, 'manifest.json')
   const outputPath = path.join('output.json')
   const requestedCalls = JSON.parse(fs.readFileSync(inputsPath, 'utf8'))
+  // const manifestInputs = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'))
+  // TODO update placeholder
+  const manifestInputs = { firstStaticNumber: 2, secondStaticNumber: 3 }
 
   const environment = generateEnvironment(requestedCalls)
-  const imports = generateEnvironmentImports(environment, requestedCalls)
+  const imports = generateEnvironmentImports(environment, requestedCalls, manifestInputs)
 
   try {
     const wasmBuffer = fs.readFileSync(wasmPath)
@@ -36,14 +40,19 @@ function generateEnvironment(requestedCalls: string[]): Environment {
   return environment
 }
 
-function generateEnvironmentImports(environment: Environment, requestedCalls: string[]): WebAssembly.Imports {
-  const imports: { [key: string]: (...args: never) => unknown } = {}
+function generateEnvironmentImports(
+  environment: Environment,
+  requestedCalls: string[],
+  inputs: WebAssembly.ModuleImports
+): WebAssembly.Imports {
+  const imports: WebAssembly.ModuleImports = {}
   requestedCalls.forEach((requestedCall) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const prop = (environment as any)[requestedCall]
+    const prop = environment[requestedCall as keyof Environment]
     if (prop === undefined || typeof prop !== 'function') throw new Error(`Invalid requested call "${requestedCall}"`)
     imports[`environment.${requestedCall}`] = prop.bind(environment)
   })
+
+  for (const [key, value] of Object.entries(inputs)) imports[`input.${key}`] = value
 
   return { index: imports }
 }
