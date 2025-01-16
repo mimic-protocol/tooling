78a8589d8ed2ef36feac07f6de883f9ba2278f71
ncomerci <ncomerci@itba.edu.ar>
2025-01-16 14:34:32 -0300

    chore: remove unnecessary zero address code

--- packages/lib-ts/common/Address.ts
+++ packages/lib-ts/common/Address.ts
@@ -23,10 +23,6 @@ export class Address extends Bytes {
   static zero(): Address {
     const self = new ByteArray(20)
 
-    for (let i = 0; i < 20; i++) {
-      self[i] = 0
-    }
-
     return changetype<Address>(self)
   }
 }
