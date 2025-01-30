cf2086570edc964138fbfe80b4aed612d801280c
PedroAraoz <paraoz22@gmail.com>
2025-01-30 11:19:48 -0300

    change char for input in deploy command

--- packages/cli/src/commands/deploy.ts
+++ packages/cli/src/commands/deploy.ts
@@ -7,7 +7,7 @@ export default class Deploy extends Command {
 
   static override flags = {
     key: Flags.string({ char: 'k', description: 'Your account deployment key' }),
-    input: Flags.string({ char: 'd', description: 'Directory containing the compiled artifacts', default: './build' }),
+    input: Flags.string({ char: 'i', description: 'Directory containing the compiled artifacts', default: './build' }),
     output: Flags.string({ char: 'o', description: 'Output directory for deployment CID', default: './build' }),
   }
 
