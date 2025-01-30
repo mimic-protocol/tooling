4c809c290d0074c6ec39b05184486a088dc8565b
Pedro <22825402+PedroAraoz@users.noreply.github.com>
2025-01-30 09:11:56 -0300

    Cli: Validate manifest structure (#9)
    Co-authored-by: Facu Spagnuolo <facundo_spagnuolo@icloud.com>

--- packages/cli/tests/commands/compile.spec.ts
+++ packages/cli/tests/commands/compile.spec.ts
@@ -1,6 +1,7 @@
 import { runCommand } from '@oclif/test'
 import { expect } from 'chai'
 import * as fs from 'fs'
+import * as path from 'path'
 
 describe('compile', () => {
   const basePath = `${__dirname}/../fixtures`
@@ -50,24 +51,66 @@ describe('compile', () => {
 
           expect(error?.message).to.be.equal('AssemblyScript compilation failed')
           expect(error?.code).to.be.equal('BuildError')
-          expect(error?.suggestions?.length).to.be.gt(0)
+          expect(error?.suggestions?.length).to.be.eq(1)
         })
       })
     })
 
     context('when the manifest is not valid', () => {
-      // skipped because there is no implementation yet
-      it.skip('throws an error', async () => {
-        const { error } = await runCommand([
-          'compile',
-          `--task ${taskPath}`,
-          `--manifest ${basePath}/manifests/invalid-manifest.yaml`,
-          `--output ${outputDir}`,
-        ])
+      context('when the manfiest has invalid fields', () => {
+        it('throws an error', async () => {
+          const { error } = await runCommand([
+            'compile',
+            `--task ${taskPath}`,
+            `--manifest ${basePath}/manifests/invalid-manifest.yaml`,
+            `--output ${outputDir}`,
+          ])
+          expect(error?.message).to.be.equal('More than one entry')
+          expect(error?.code).to.be.equal('MoreThanOneEntryError')
+          expect(error?.suggestions?.length).to.be.eq(1)
+        })
+      })
 
-        expect(error?.message).to.be.equal('Invalid Manifest')
-        expect(error?.code).to.be.equal('InvalidManifest')
-        expect(error?.suggestions?.length).to.be.gt(0)
+      context('when the manfiest has repeated fields', () => {
+        it('throws an error', async () => {
+          const { error } = await runCommand([
+            'compile',
+            `--task ${taskPath}`,
+            `--manifest ${basePath}/manifests/invalid-manifest-repeated.yaml`,
+            `--output ${outputDir}`,
+          ])
+          expect(error?.message).to.be.equal('Duplicate Entry')
+          expect(error?.code).to.be.equal('DuplicateEntryError')
+          expect(error?.suggestions?.length).to.be.eq(1)
+        })
+      })
+
+      context('when the manifest is incomplete', () => {
+        it('throws an error', async () => {
+          const { error } = await runCommand([
+            'compile',
+            `--task ${taskPath}`,
+            `--manifest ${basePath}/manifests/incomplete-manifest.yaml`,
+            `--output ${outputDir}`,
+          ])
+          expect(error?.message).to.be.equal('Missing/Incorrect Fields')
+          expect(error?.code).to.be.equal('FieldsError')
+          expect(error?.suggestions?.length).to.be.eq(3)
+        })
+      })
+
+      context('when the manifest is empty', () => {
+        it('throws an error', async () => {
+          const { error } = await runCommand([
+            'compile',
+            `--task ${taskPath}`,
+            `--manifest ${basePath}/manifests/empty-manifest.yaml`,
+            `--output ${outputDir}`,
+          ])
+          expect(error?.message).to.be.equal('Empty Manifest')
+          expect(error?.code).to.be.equal('EmptyManifestError')
+          expect(error?.suggestions?.length).to.be.eq(1)
+        })
       })
     })
   })
@@ -85,7 +128,38 @@ describe('compile', () => {
 
       expect(error?.message).to.be.equal(`Could not find ${inexistentManifestPath}`)
       expect(error?.code).to.be.equal('FileNotFound')
-      expect(error?.suggestions?.length).to.be.gt(0)
+      expect(error?.suggestions?.length).to.be.eq(1)
+    })
+  })
+
+  context('when the output directory already exists', () => {
+    beforeEach('create outputDirectory with files', () => {
+      if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true })
+      fs.writeFileSync(path.join(outputDir, 'inputs.json'), JSON.stringify({ a: 2 }, null, 2))
+      fs.writeFileSync(path.join(outputDir, 'randomFile.txt'), JSON.stringify({ a: 2 }, null, 2))
+    })
+
+    it('creates the files correctly', async () => {
+      const { stdout, error } = await runCommand([
+        'compile',
+        `--task ${taskPath}`,
+        `--manifest ${manifestPath}`,
+        `--output ${outputDir}`,
+      ])
+
+      expect(error).to.be.undefined
+      expect(stdout).to.include('Build complete!')
+
+      expect(fs.existsSync(`${outputDir}/task.wasm`)).to.be.true
+      expect(fs.existsSync(`${outputDir}/task.wat`)).to.be.true
+      expect(fs.existsSync(`${outputDir}/inputs.json`)).to.be.true
+      expect(fs.existsSync(`${outputDir}/manifest.json`)).to.be.true
+
+      const inputs = JSON.parse(fs.readFileSync(`${outputDir}/inputs.json`, 'utf-8'))
+      const manifest = JSON.parse(fs.readFileSync(`${outputDir}/manifest.json`, 'utf-8'))
+
+      expect(inputs).to.be.deep.equal(['getValue', 'createIntent'])
+      expect(manifest.inputs).to.be.deep.equal({ firstStaticNumber: 2, secondStaticNumber: 3 })
     })
   })
 })
