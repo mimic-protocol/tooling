589c639814119322dd66c8a17ab70f9d20f1d8fe
ncomerci <ncomerci@itba.edu.ar>
2025-02-14 14:19:17 -0300

    chore: remove comments

--- packages/cli/tests/fixtures/tasks/invalid-task.ts
+++ packages/cli/tests/fixtures/tasks/invalid-task.ts
@@ -1,11 +1,3 @@
-// import { environment } from '@mimicprotocol/lib-ts'
-
-// export default function main(): void {
-//   const firstNumber = environment.getValue()
-//   const firstIntent = firstNumber * undeclaredVariable
-//   environment.createIntent(firstIntent)
-// }
-
 import { Address, BigInt, Bytes, environment, NULL_ADDRESS } from '@mimicprotocol/lib-ts'
 
 export default function main(): void {
--- packages/cli/tests/fixtures/tasks/task.ts
+++ packages/cli/tests/fixtures/tasks/task.ts
@@ -1,19 +1,8 @@
 /* eslint-disable @typescript-eslint/no-namespace */
-// import { environment } from '@mimicprotocol/lib-ts'
-
 declare namespace input {
   const firstStaticNumber: i32
   const secondStaticNumber: i32
 }
-// export default function main(): void {
-//   const firstNumber = environment.getValue()
-//   const firstIntent = firstNumber * input.firstStaticNumber
-//   environment.createIntent(firstIntent)
-
-//   const secondNumber = environment.getValue()
-//   const secondIntent = secondNumber * input.secondStaticNumber
-//   environment.createIntent(secondIntent)
-// }
 
 import { Address, BigInt, Bytes, environment, NULL_ADDRESS } from '@mimicprotocol/lib-ts'
 
