1cded83ff2d806cebf204f7b9824808d6e9e4b0e
PedroAraoz <paraoz22@gmail.com>
2025-02-27 18:46:59 -0300

    add test

---
+++ packages/lib-ts/tests/helpers/serialize.spec.ts
@@ -0,0 +1,62 @@
+import { Address, BigInt, Bytes } from "../../common"
+import { join, serialize } from "../../helpers"
+import { NULL_ADDRESS } from "../helpers"
+
+describe('serialize', () => {
+  describe('serialize', () => {
+    describe('when passing an Address', () => {
+      it('converts it to string correctly', () => {
+        const address = Address.fromString(NULL_ADDRESS)
+        const serialized = serialize(address)
+        expect(serialized).toBe(NULL_ADDRESS)
+      })
+    })
+
+    describe('when passing Bytes', () => {
+      it('converts it to string correctly', () => {
+        const bytes = Bytes.fromI32(5)
+        const serialized = serialize(bytes)
+        expect(serialized).toBe("0x05000000")
+      })
+    })
+
+    describe('when passing a BigInt', () => {
+      it('converts it to string correctly', () => {
+        const bigInt = BigInt.fromI32(5)
+        const serialized = serialize(bigInt)
+        expect(serialized).toBe("BigInt(5)")
+      })
+    })
+
+    describe('when passing a number', () => {
+      it('converts it to string correctly', () => {
+        const num = 5
+        const serialized = serialize(5)
+        expect(serialized).toBe("5")
+      })
+    })
+  })
+
+  describe('join', () => {
+    describe('when passing a list of strings', () => {
+      it('returns them joined', () => {
+        const joint = join(["one", "two", "three"])
+        expect(joint).toBe("one,two,three")
+      })
+    })
+
+    describe('when passing a list of nulls', () => {
+      it('returns them joined', () => {
+        const joint = join([null, null, null])
+        expect(joint).toBe(",,")
+      })
+    })
+
+    describe('when passing a mixed list', () => {
+      it('returns them joined', () => {
+        const joint = join(["one", null, "three"])
+        expect(joint).toBe("one,,three")
+      })
+    })
+  })
+})
