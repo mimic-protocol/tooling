545e8ff5fcd9d8abe5d7245c46d98eefa0206baa
ncomerci <ncomerci@itba.edu.ar>
2025-03-27 12:12:06 -0300

    chore: remove relevant tokens related code

--- packages/lib-ts/as-pect.config.js
+++ packages/lib-ts/as-pect.config.js
@@ -18,7 +18,6 @@ export default {
     let exports // Imports can reference this
 
     const tokenPrices = new Map()
-    const relevantTokens = new Map()
 
     const myImports = {
       env: {
@@ -41,20 +40,6 @@ export default {
 
           return exports.__newString(price)
         },
-        _getRelevantTokens: (paramsPtr) => {
-          const paramsStr = exports.__getString(paramsPtr)
-          const params = parseCSV(paramsStr)
-          const address = params[0]
-          const chainIds = parseCSV(params[1].split('Array(')[1].split(')')[0])
-
-          let responseStr = ''
-          for (const chainId of chainIds) {
-            const response = relevantTokens.get(`${address}:${chainId}`) ?? []
-            responseStr += response.join('\n')
-          }
-
-          return exports.__newString(responseStr)
-        },
       },
       helpers: {
         _setTokenPrice: (addressPtr, chainId, pricePtr) => {
@@ -62,14 +47,6 @@ export default {
           const price = exports.__getString(pricePtr)
           const key = `${address}:${chainId}`
           tokenPrices.set(key, price)
-        },
-        _setRelevantToken: (addressPtr, chainId, tokenAmountPtr) => {
-          const address = exports.__getString(addressPtr)
-          const tokenAmount = exports.__getString(tokenAmountPtr)
-          const key = `${address}:${chainId}`
-          const userTokens = relevantTokens.get(key) ?? []
-          userTokens.push(tokenAmount)
-          relevantTokens.set(key, userTokens)
         },
       },
     }
@@ -84,57 +61,4 @@ export default {
    * Specify if the binary wasm file should be written to the file system.
    */
   outputBinary: false,
-}
-
-function parseCSV(csvString) {
-  const SEPARATOR = ','
-  const tokens = []
-  const currentTokenChars = []
-  let parenthesisDepth = 0
-  let isEmpty = true
-
-  for (let i = 0; i < csvString.length; i++) {
-    const char = csvString.charAt(i)
-
-    switch (char) {
-      case '(':
-        parenthesisDepth++
-        currentTokenChars.push(char)
-        isEmpty = false
-        break
-
-      case ')':
-        parenthesisDepth--
-        if (parenthesisDepth < 0) throw new Error(`Unbalanced brackets at position ${i}`)
-        currentTokenChars.push(char)
-        isEmpty = false
-        break
-
-      case SEPARATOR:
-        if (parenthesisDepth === 0) {
-          isEmpty ? tokens.push(null) : tokens.push(currentTokenChars.join(''))
-          currentTokenChars.length = 0
-          isEmpty = true
-        } else {
-          currentTokenChars.push(char)
-          isEmpty = false
-        }
-        break
-
-      default:
-        currentTokenChars.push(char)
-        isEmpty = false
-        break
-    }
-  }
-
-  if (parenthesisDepth !== 0) {
-    throw new Error('Unbalanced brackets at the end of the string')
-  }
-
-  if (csvString.length > 0) {
-    isEmpty ? tokens.push(null) : tokens.push(currentTokenChars.join(''))
-  }
-
-  return tokens
 }
