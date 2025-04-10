1f10094eae4f60371cf769bfa5b293318a7552fa
ncomerci <ncomerci@itba.edu.ar>
2025-04-10 14:24:12 -0300

    cli: improve build & init commands

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,5 +1,6 @@
 import { confirm } from '@inquirer/prompts'
 import { Command, Flags } from '@oclif/core'
+import { spawnSync } from 'child_process'
 import * as fs from 'fs'
 import * as path from 'path'
 
@@ -47,12 +48,48 @@ export default class Init extends Command {
       })
     }
 
-    const srcPath = path.join(fullDirectory, 'src/')
-    const manifestPath = path.join(fullDirectory, 'manifest.yaml')
-    fs.mkdirSync(srcPath, { recursive: true })
-    fs.copyFileSync(`${templateDirectory}/task.ts`, path.join(srcPath, 'task.ts'))
-    fs.copyFileSync(`${templateDirectory}/manifest.yaml`, manifestPath)
+    fs.mkdirSync(fullDirectory, { recursive: true })
+    this.copyDirectory(templateDirectory, fullDirectory)
+
+    // NOTE: This is a temporary solution to link the Mimic library, in the future, lib-ts should be a dependency
+    log.startAction('Linking Mimic libraries')
+    spawnSync('yarn', ['link', '@mimicprotocol/lib-ts'], {
+      cwd: fullDirectory,
+      stdio: 'inherit',
+    })
     log.stopAction()
+
+    log.startAction('Installing dependencies')
+    spawnSync('yarn', ['install'], {
+      cwd: fullDirectory,
+      stdio: 'inherit',
+    })
+    log.stopAction()
+
+    log.startAction('Initializing git repository')
+    spawnSync('git', ['init'], {
+      cwd: fullDirectory,
+      stdio: 'inherit',
+    })
+    log.stopAction()
+
     console.log('New project initialized!')
+  }
+
+  private copyDirectory(source: string, destination: string): void {
+    fs.mkdirSync(destination, { recursive: true })
+
+    const entries = fs.readdirSync(source, { withFileTypes: true })
+
+    for (const entry of entries) {
+      const srcPath = path.join(source, entry.name)
+      const destPath = path.join(destination, entry.name)
+
+      if (entry.isDirectory()) {
+        this.copyDirectory(srcPath, destPath)
+      } else {
+        fs.copyFileSync(srcPath, destPath)
+      }
+    }
   }
 }
