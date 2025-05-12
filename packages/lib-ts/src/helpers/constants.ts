ce3be25ea6daadc662406b34d732648dfa741e62
Tintoretto <agustinlavarello@gmail.com>
2025-05-12 14:24:37 +0100

    Lib: Remove unused EVM_ENCODE_SLOT_SIZE helper constant (#57)

--- packages/lib-ts/src/helpers/constants.ts
+++ packages/lib-ts/src/helpers/constants.ts
@@ -1,8 +1,6 @@
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000'
 export const NATIVE_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 export const STANDARD_DECIMALS: u8 = 18
-export const EVM_ENCODE_SLOT_SIZE: i32 = 32
-
 export enum ListType {
   AllowList = 0,
   DenyList = 1,
