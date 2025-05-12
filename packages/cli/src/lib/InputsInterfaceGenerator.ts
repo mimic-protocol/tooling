bed88ffc5d509e48b9b32721b021aa338284d0ea
lgalende <lau@mimic.fi>
2025-05-12 13:41:35 -0300

    cli: read input strings from memory

--- packages/cli/src/lib/InputsInterfaceGenerator.ts
+++ packages/cli/src/lib/InputsInterfaceGenerator.ts
@@ -4,12 +4,21 @@ export default {
     const convertedInputs = convertInputs(inputs)
     const inputsMapping = generateInputsMapping(convertedInputs)
     const imports = generateImports(convertedInputs)
-    return `
-${imports}
-
-export declare namespace input {
-  ${inputsMapping}
-}`.trim()
+    const inputsClass = generateInputsClass(convertedInputs)
+    return [
+      imports,
+      '',
+      'declare namespace input {',
+      `  ${inputsMapping}`,
+      '}',
+      '',
+      '// The class name is intentionally lowercase and plural to resemble a namespace when used in a task',
+      'export class inputs {',
+      `  ${inputsClass}`,
+      '}',
+    ]
+      .join('\n')
+      .trim()
   },
 }
 
@@ -25,8 +34,27 @@ function generateImports(inputs: Record<string, string>): string {
 
 function generateInputsMapping(inputs: Record<string, string>): string {
   return Object.entries(inputs)
-    .map(([name, type]) => `const ${name}: ${type}`)
+    .map(([name, type]) =>
+      type === 'string' || type === 'Address' || type === 'Bytes' ? `var ${name}Ptr: u32` : `const ${name}: ${type}`
+    )
     .join('\n  ')
+}
+
+function generateInputsClass(inputs: Record<string, string>): string {
+  return Object.entries(inputs)
+    .map(([name, type]) => {
+      switch (type) {
+        case 'string':
+          return generateStringGetter(name)
+        case 'Address':
+          return generateHexGetter(name, type, 'Address.fromString')
+        case 'Bytes':
+          return generateHexGetter(name, type, 'Bytes.fromHexString')
+        default:
+          return generateDefaultGetter(name, type)
+      }
+    })
+    .join('\n\n  ')
 }
 
 function convertType(type: string): string {
@@ -36,4 +64,23 @@ function convertType(type: string): string {
   if (type.includes('bytes')) type = 'Bytes'
   type = type.replace(/128|256/gm, '64')
   return type
+}
+
+function generateStringGetter(name: string): string {
+  return `static get ${name}(): string {
+    return changetype<string>(input.${name}Ptr)
+  }`
+}
+
+function generateHexGetter(name: string, type: string, fn: string): string {
+  return `static get ${name}(): ${type} {
+    const str = changetype<string>(input.${name}Ptr)
+    return ${fn}(str)
+  }`
+}
+
+function generateDefaultGetter(name: string, type: string): string {
+  return `static get ${name}(): ${type} {
+    return input.${name}
+  }`
 }
--- packages/integration/tests/003-multiple-call-intents/src/types/index.ts
+++ packages/integration/tests/003-multiple-call-intents/src/types/index.ts
@@ -1,3 +1,10 @@
-export declare namespace input {
+declare namespace input {
   const chainId: i32
+}
+
+// The class name is intentionally lowercase and plural to resemble a namespace when used in a task
+export class inputs {
+  static get chainId(): i32 {
+    return input.chainId
+  }
 }
