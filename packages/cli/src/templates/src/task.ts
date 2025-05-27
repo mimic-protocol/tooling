a3084920f4fc57910874562f8f4ca664ab50936e
Tintoretto <agustinlavarello@gmail.com>
2025-05-27 18:18:52 +0200

    CLI: Fix mimic init (#68)

--- packages/cli/src/templates/src/task.ts
+++ packages/cli/src/templates/src/task.ts
@@ -1,13 +1,11 @@
-import { Address, BigInt, Bytes, environment, NULL_ADDRESS } from '@mimicprotocol/lib-ts'
+import { Address, BigInt, Bytes, CallData, environment, NULL_ADDRESS } from '@mimicprotocol/lib-ts'
 
 export default function main(): void {
-  const settler = Address.fromString(NULL_ADDRESS)
-  const chainId = 1
   const target = Address.fromString(NULL_ADDRESS)
   const data = Bytes.empty()
   const feeToken = Address.fromString(NULL_ADDRESS)
   const feeAmount = BigInt.zero()
 
   // Replace this with your task code
-  environment.call(settler, chainId, target, feeToken, feeAmount, data)
+  environment.call([new CallData(target, data)], feeToken, feeAmount)
 }
