3fe5753a1792d6721cdb4b0ad540d3d774c1fad2
ncomerci <ncomerci@itba.edu.ar>
2025-06-09 15:56:12 -0300

    fix: template

--- packages/cli/src/templates/src/task.ts
+++ packages/cli/src/templates/src/task.ts
@@ -1,11 +1,12 @@
-import { Address, BigInt, Bytes, CallData, environment, NULL_ADDRESS } from '@mimicprotocol/lib-ts'
+import { Address, BigInt, Bytes, CallData, environment, NULL_ADDRESS, Token, TokenAmount } from '@mimicprotocol/lib-ts'
 
 export default function main(): void {
+  const settler = Address.fromString(NULL_ADDRESS)
   const target = Address.fromString(NULL_ADDRESS)
   const data = Bytes.empty()
-  const feeToken = Address.fromString(NULL_ADDRESS)
-  const feeAmount = BigInt.zero()
+  const feeToken = new Token(NULL_ADDRESS, 1, 18, 'TEST')
+  const feeTokenAmount = new TokenAmount(feeToken, BigInt.zero())
 
   // Replace this with your task code
-  environment.call([new CallData(target, data)], feeToken, feeAmount)
+  environment.call([new CallData(target, data)], feeTokenAmount, settler)
 }
