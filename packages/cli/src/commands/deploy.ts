4e2d41a3379cbd07d59fb1da196a42b45e9c1f71
Pedro <22825402+PedroAraoz@users.noreply.github.com>
2025-06-17 20:03:05 -0300

    CLI: Improve deploy error messages (#77)
    Co-authored-by: lgalende <lau@mimic.fi>

--- packages/cli/src/commands/deploy.ts
+++ packages/cli/src/commands/deploy.ts
@@ -4,7 +4,7 @@ import FormData from 'form-data'
 import * as fs from 'fs'
 import { join, resolve } from 'path'
 
-import { GENERIC_SUGGESTION, RegistryPartialError } from '../errors'
+import { GENERIC_SUGGESTION } from '../errors'
 import log from '../log'
 
 const MIMIC_REGISTRY = 'https://api-protocol.mimic.fi'
@@ -30,7 +30,7 @@ export default class Deploy extends Command {
 
     if (!fs.existsSync(fullInputDir))
       this.error(`Directory ${log.highlightText(fullInputDir)} does not exist`, {
-        code: 'DirectoryNotFound',
+        code: 'Directory Not Found',
         suggestions: ['Use the --input flag to specify the correct path'],
       })
 
@@ -38,7 +38,7 @@ export default class Deploy extends Command {
     for (const file of neededFiles) {
       if (!fs.existsSync(file))
         this.error(`Could not find ${file}`, {
-          code: 'FileNotFound',
+          code: 'File Not Found',
           suggestions: [`Use ${log.highlightText('mimic compile')} to generate the needed files`],
         })
     }
@@ -63,7 +63,6 @@ export default class Deploy extends Command {
           'Content-Type': `multipart/form-data; boundary=${form.getBoundary()}`,
         },
       })
-      if (data.errorMessage) throw new RegistryPartialError(data.errorMessage)
       return data.CID
     } catch (err) {
       this.handleError(err, 'Failed to upload to registry')
@@ -71,15 +70,15 @@ export default class Deploy extends Command {
   }
 
   private handleError(err: unknown, message: string): never {
-    if (err instanceof RegistryPartialError)
-      this.error(message, { code: 'RegistrationError', suggestions: GENERIC_SUGGESTION })
     if (!(err instanceof AxiosError)) this.error(err as Error)
     const statusCode = err.response?.status
-    if (statusCode === 401) this.error(`${message}`, { code: 'Unauthorized', suggestions: ['Review your key'] })
-    this.error(`${message} - ${err.message}`, {
-      code: `${err.response?.status}Error`,
-      suggestions: GENERIC_SUGGESTION,
-    })
+    if (statusCode === 400) {
+      const errMessage = err.response?.data?.content?.message || message
+      this.error(errMessage, { code: 'Bad Request', suggestions: ['Review the uploaded files'] })
+    }
+    if (statusCode === 401) this.error(message, { code: 'Unauthorized', suggestions: ['Review your key'] })
+    if (statusCode === 403) this.error(message, { code: 'Invalid api key', suggestions: ['Review your key'] })
+    this.error(`${message} - ${err.message}`, { code: `${statusCode} Error`, suggestions: GENERIC_SUGGESTION })
   }
 }
 
