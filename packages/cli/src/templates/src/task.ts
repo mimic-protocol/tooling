2ca0ef8a3780e158c94b44798ee413471826dc5d
ncomerci <ncomerci@itba.edu.ar>
2025-06-17 13:44:56 -0300

    fix: lint

--- packages/cli/src/templates/src/task.ts
+++ packages/cli/src/templates/src/task.ts
@@ -1,4 +1,13 @@
-import { Address, BigInt, Bytes, CallBuilder, environment, NULL_ADDRESS, Token, TokenAmount } from '@mimicprotocol/lib-ts'
+import {
+  Address,
+  BigInt,
+  Bytes,
+  CallBuilder,
+  environment,
+  NULL_ADDRESS,
+  Token,
+  TokenAmount,
+} from '@mimicprotocol/lib-ts'
 
 export default function main(): void {
   const settler = Address.fromString(NULL_ADDRESS)
@@ -8,7 +17,7 @@ export default function main(): void {
   const feeToken = new Token(NULL_ADDRESS, chainId, 18, 'TEST')
   const feeTokenAmount = new TokenAmount(feeToken, BigInt.zero())
   const callBuilder = new CallBuilder(feeTokenAmount, chainId).addCall(target, data).addSettler(settler)
-  
+
   // Replace this with your task code
   environment.call(callBuilder.build())
 }
