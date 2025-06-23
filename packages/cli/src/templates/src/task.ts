3578ce89b964764bf4a3b12b656679a31aaed8d4
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2025-06-23 18:55:02 -0300

    Lib: Improve init template (#84)

--- packages/cli/src/templates/src/task.ts
+++ packages/cli/src/templates/src/task.ts
@@ -1,23 +1,7 @@
-import {
-  Address,
-  BigInt,
-  Bytes,
-  CallBuilder,
-  environment,
-  NULL_ADDRESS,
-  Token,
-  TokenAmount,
-} from '@mimicprotocol/lib-ts'
+import { Transfer } from '@mimicprotocol/lib-ts'
+
+import { inputs } from './types'
 
 export default function main(): void {
-  const chainId = 1
-  const settler = Address.fromString(NULL_ADDRESS)
-  const target = Address.fromString(NULL_ADDRESS)
-  const data = Bytes.empty()
-  const feeToken = Token.fromString(NULL_ADDRESS, chainId, 18, 'TEST')
-  const fee = TokenAmount.fromBigInt(feeToken, BigInt.zero())
-  const callBuilder = CallBuilder.forChainWithFee(chainId, fee).addCall(target, data).addSettler(settler)
-
-  // Replace this with your task code
-  environment.call(callBuilder.build())
+  Transfer.create(inputs.chainId, inputs.token, inputs.amount, inputs.recipient, inputs.fee).send()
 }
