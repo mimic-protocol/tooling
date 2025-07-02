de6545fc3c8f854a15f1aad53897078a8704d204
lgalende <63872655+lgalende@users.noreply.github.com>
2025-07-02 10:09:39 -0300

    Test: Add task testing support (#93)
    Co-authored-by: Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>

---
+++ packages/cli/src/commands/test.ts
@@ -0,0 +1,31 @@
+import { Command, Flags } from '@oclif/core'
+import { spawnSync } from 'child_process'
+import * as path from 'path'
+
+export default class Test extends Command {
+  static override description = 'Runs task tests'
+
+  static override examples = ['<%= config.bin %> <%= command.id %> --directory ./']
+
+  static override flags = {
+    directory: Flags.string({ char: 'd', description: 'task directory', default: './' }),
+    skipCompile: Flags.boolean({ description: 'skip codegen and compile steps' }),
+  }
+
+  public async run(): Promise<void> {
+    const { flags } = await this.parse(Test)
+    const baseDir = path.resolve(flags.directory)
+    const testPath = path.join(baseDir, 'tests')
+
+    if (!flags.skipCompile) {
+      const codegen = spawnSync('yarn', ['mimic', 'codegen'], { cwd: baseDir, stdio: 'inherit' })
+      if (codegen.status !== 0) return
+
+      const compile = spawnSync('yarn', ['mimic', 'compile'], { cwd: baseDir, stdio: 'inherit' })
+      if (compile.status !== 0) return
+    }
+    spawnSync('yarn', ['tsx', './node_modules/mocha/bin/mocha.js', `${testPath}/**/*.spec.ts`], {
+      stdio: 'inherit',
+    })
+  }
+}
