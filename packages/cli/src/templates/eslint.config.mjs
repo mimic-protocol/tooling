8bc4bc7de2442d53ad89bdac603dea99ce520817
ncomerci <ncomerci@itba.edu.ar>
2025-07-10 11:55:43 -0300

    chore: ignore default types path in mimic init lint

--- packages/cli/src/templates/eslint.config.mjs
+++ packages/cli/src/templates/eslint.config.mjs
@@ -7,7 +7,7 @@ import eslintPluginPrettier from "eslint-plugin-prettier"
 
 export default [
   {
-    ignores: ["node_modules/**", "**/dist/**", "**/build/**", "**/.prettierrc.*"]
+    ignores: ["node_modules/**", "**/dist/**", "**/build/**", "**/.prettierrc.*", "./src/types/**"]
   },
   {
     files: ["**/*.{ts,tsx}"],
