da2f8075a5048c1efb346cf7c543c83d6ddd809b
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2025-08-14 19:25:23 -0300

    Lib: Add support for sonic chain (#113)

---
+++ packages/lib-ts/src/chains/Sonic.ts
@@ -0,0 +1,13 @@
+import { ERC20Token } from '../tokens'
+import { ChainId } from '../types'
+
+/* eslint-disable no-secrets/no-secrets */
+
+export namespace Sonic {
+  export const CHAIN_ID = ChainId.SONIC
+  export const SONIC = ERC20Token.native(CHAIN_ID)
+  export const USDC = ERC20Token.fromString('0x29219dd400f2Bf60E5a23d13Be72B486D4038894', CHAIN_ID, 6, 'USDC')
+  export const USDT = ERC20Token.fromString('0x6047828dc181963ba44974801ff68e538da5eaf9', CHAIN_ID, 6, 'USDT')
+  export const WETH = ERC20Token.fromString('0x50c42dEAcD8Fc9773493ED674b675bE577f2634b', CHAIN_ID, 18, 'WETH')
+  export const WSONIC = ERC20Token.fromString('0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38', CHAIN_ID, 18, 'WSONIC')
+}
