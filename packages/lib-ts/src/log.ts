4a589a3171858fae7ec495a33b7ce6eacce77224
ncomerci <ncomerci@itba.edu.ar>
2025-09-24 14:58:06 -0300

    feat: improve log function

--- packages/lib-ts/src/log.ts
+++ packages/lib-ts/src/log.ts
@@ -4,6 +4,8 @@
 // Copyright (c) 2018 Graph Protocol, Inc. and contributors.
 // Modified by Mimic Protocol, 2025.
 
+import { Stringable } from "./helpers"
+
 export namespace log {
   @external('log', '_log')
   declare function _log(level: Level, msg: string): void
@@ -22,7 +24,7 @@ export namespace log {
    * @param msg Format string like "Value = {}, other = {}".
    * @param args Format string arguments.
    */
-  export function critical(msg: string, args: Array<string> = []): void {
+  export function critical<T extends Stringable>(msg: string, args: Array<T> = []): void {
     _log(Level.CRITICAL, format(msg, args))
   }
 
@@ -32,7 +34,7 @@ export namespace log {
    * @param msg Format string like "Value = {}, other = {}".
    * @param args Format string arguments.
    */
-  export function error(msg: string, args: Array<string> = []): void {
+  export function error<T extends Stringable>(msg: string, args: Array<T> = []): void {
     _log(Level.ERROR, format(msg, args))
   }
 
@@ -41,7 +43,7 @@ export namespace log {
    * @param msg Format string like "Value = {}, other = {}".
    * @param args Format string arguments.
    */
-  export function warning(msg: string, args: Array<string> = []): void {
+  export function warning<T extends Stringable>(msg: string, args: Array<T> = []): void {
     _log(Level.WARNING, format(msg, args))
   }
 
@@ -50,7 +52,7 @@ export namespace log {
    * @param msg Format string like "Value = {}, other = {}".
    * @param args Format string arguments.
    */
-  export function info(msg: string, args: Array<string> = []): void {
+  export function info<T extends Stringable>(msg: string, args: Array<T> = []): void {
     _log(Level.INFO, format(msg, args))
   }
 
@@ -59,20 +61,21 @@ export namespace log {
    * @param msg Format string like "Value = {}, other = {}".
    * @param args Format string arguments.
    */
-  export function debug(msg: string, args: Array<string> = []): void {
+  export function debug<T extends Stringable>(msg: string, args: Array<T> = []): void {
     _log(Level.DEBUG, format(msg, args))
   }
 }
 
-function format(fmt: string, args: string[]): string {
+function format<T extends Stringable>(fmt: string, args: Array<T>): string {
   let out = ''
   let argIndex = 0
+  const argsStr = args.map<string>(a => a.toString())
   for (let i: i32 = 0, len: i32 = fmt.length; i < len; i++) {
     if (i < len - 1 && fmt.charCodeAt(i) == 0x7b /* '{' */ && fmt.charCodeAt(i + 1) == 0x7d /* '}' */) {
-      if (argIndex >= args.length) {
+      if (argIndex >= argsStr.length) {
         throw new Error('Too few arguments for format string: ' + fmt)
       } else {
-        out += args[argIndex++]
+        out += argsStr[argIndex++]
         i++
       }
     } else {
