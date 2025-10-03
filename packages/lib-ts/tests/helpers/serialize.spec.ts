0cb9994e4e51e6f923512dee75f18ca0a26150cb
Tintoretto <agus@mimic.fi>
2025-10-03 17:01:38 -0300

    Lib: Add event trigger decoding (#138)
    Co-authored-by: lgalende <63872655+lgalende@users.noreply.github.com>

--- packages/lib-ts/tests/helpers/serialize.spec.ts
+++ packages/lib-ts/tests/helpers/serialize.spec.ts
@@ -1,4 +1,5 @@
-import { deserializeCronTriggerData, NULL_ADDRESS, serialize } from '../../src/helpers'
+import { Trigger } from '../../src/context'
+import { NULL_ADDRESS, serialize } from '../../src/helpers'
 import { Address, BigInt, Bytes } from '../../src/types'
 import { setEvmDecode } from '../helpers'
 
@@ -47,8 +48,28 @@ describe('serialize', () => {
     it('decodes a uint256 correctly', () => {
       const data = '0x0000000000000000000000000000000000000000000000000000019962bc6d60'
       setEvmDecode('uint256', '0x0000000000000000000000000000000000000000000000000000019962bc6d60', '1758298140000')
-      const deserialized = deserializeCronTriggerData(data)
+      const deserialized = Trigger.deserializeCronTriggerData(data)
       expect(deserialized.toString()).toBe('1758298140000')
+    })
+  })
+
+  describe('deserializeEventTriggerData', () => {
+    it('decodes the event correctly', () => {
+      const data =
+        '0x0000000000000000000000000000000000000000000000000000000000000020e171661125168440ba1f1eb114291cbbe6f55e99f7d1612e9f6786d3941049720000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000001dcf810f941d3680038937e197031e3fd9716dbe6aa0c31ea2ad35cf4c457a6cc0000000000000000000000000000000000000000000000000000000000000040380ad672d259b4a3ae2d55e3704eafcefffc1e69dcd9cf952dd14670cd28e81324e9127ac0c37b8e8c140116fd30fa5f9e96b0e9e680e914614889f12896123c'
+      setEvmDecode(
+        '(bytes32,uint256,bytes32[],bytes)',
+        data,
+        '["0xe171661125168440ba1f1eb114291cbbe6f55e99f7d1612e9f6786d394104972","2","[\\"0xdcf810f941d3680038937e197031e3fd9716dbe6aa0c31ea2ad35cf4c457a6cc\\"]","0x380ad672d259b4a3ae2d55e3704eafcefffc1e69dcd9cf952dd14670cd28e81324e9127ac0c37b8e8c140116fd30fa5f9e96b0e9e680e914614889f12896123c"]'
+      )
+      const event = Trigger.deserializeEventTriggerData(data)
+      expect(event.blockHash).toBe('0xe171661125168440ba1f1eb114291cbbe6f55e99f7d1612e9f6786d394104972')
+      expect(event.index).toBe(BigInt.fromStringDecimal('2', 0))
+      expect(event.topics.length).toBe(1)
+      expect(event.topics[0]).toBe('0xdcf810f941d3680038937e197031e3fd9716dbe6aa0c31ea2ad35cf4c457a6cc')
+      expect(event.eventData).toBe(
+        '0x380ad672d259b4a3ae2d55e3704eafcefffc1e69dcd9cf952dd14670cd28e81324e9127ac0c37b8e8c140116fd30fa5f9e96b0e9e680e914614889f12896123c'
+      )
     })
   })
 })
