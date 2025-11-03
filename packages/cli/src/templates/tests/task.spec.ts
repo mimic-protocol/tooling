2374d2530ae8d8f3b36c0f560e529901f7972d71
lgalende <lau@mimic.fi>
2025-11-03 01:02:37 -0300

    test: replace runner mock with ts package

--- packages/cli/src/templates/tests/task.spec.ts
+++ packages/cli/src/templates/tests/task.spec.ts
@@ -1,17 +1,20 @@
+import { Chains, OpType } from '@mimicprotocol/sdk'
 import { runTask, Transfer } from '@mimicprotocol/test-ts'
 import { expect } from 'chai'
 
 describe('Task', () => {
-  const taskDir = './'
+  const taskDir = './build'
+
+  const chainId = Chains.Optimism
 
   const context = {
     user: '0x756f45e3fa69347a9a973a725e3c98bc4db0b5a0',
-    settlers: [{ address: '0xdcf1d9d12a0488dfb70a8696f44d6d3bc303963d', chainId: 10 }],
+    settlers: [{ address: '0xdcf1d9d12a0488dfb70a8696f44d6d3bc303963d', chainId }],
     timestamp: Date.now(),
   }
 
   const inputs = {
-    chainId: 10, // Optimism
+    chainId,
     token: '0x7f5c764cbc14f9669b88837ca1490cca17c31607', // USDC
     amount: '1000000', // 1 USDC
     recipient: '0xbce3248ede29116e4bd18416dcc2dfca668eeb84',
@@ -19,12 +22,14 @@ describe('Task', () => {
   }
 
   it('produces the expected intents', async () => {
-    const intents = (await runTask(taskDir, context, { inputs })) as Transfer[]
+    const result = await runTask(taskDir, context, { inputs })
+    expect(result.success).to.be.true
+    expect(result.timestamp).to.be.equal(context.timestamp)
 
-    expect(intents).to.be.an('array')
+    const intents = result.intents as Transfer[]
     expect(intents).to.have.lengthOf(1)
 
-    expect(intents[0].type).to.be.equal('transfer')
+    expect(intents[0].op).to.be.equal(OpType.Transfer)
     expect(intents[0].settler).to.be.equal(context.settlers[0].address)
     expect(intents[0].user).to.be.equal(context.user)
     expect(intents[0].chainId).to.be.equal(inputs.chainId)
