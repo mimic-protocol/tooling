fca606559627e2ebe396924c8bb9724c6fc536af
Nicolás Comerci <45410089+ncomerci@users.noreply.github.com>
2025-11-04 20:04:59 -0300

    CLI: Improve init command (#159)

--- packages/cli/src/templates/src/task.ts
+++
@@ -1,8 +0,0 @@
-import { ERC20Token, Transfer } from '@mimicprotocol/lib-ts'
-
-import { inputs } from './types'
-
-export default function main(): void {
-  const token = ERC20Token.fromAddress(inputs.token, inputs.chainId)
-  Transfer.create(token, inputs.amount, inputs.recipient, inputs.maxFee).send()
-}
