d58c6d2f6af6f4eef8717f439445ad63057bd351
Agustincito <agus@poap.io>
2025-11-09 20:28:42 +0100

    Fix constant

--- packages/lib-ts/src/helpers/constants.ts
+++ packages/lib-ts/src/helpers/constants.ts
@@ -1,11 +1,9 @@
-import { BigInt } from '../..'
-
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000'
 export const EVM_NATIVE_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 export const SVM_NATIVE_ADDRESS = 'Nativeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 export const USD_ADDRESS = '0x0000000000000000000000000000000000000348'
 
-export const MAX_UINT256 = BigInt.fromHexString('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+export const MAX_UINT256_HEX = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
 
 export const STANDARD_DECIMALS: u8 = 18
 
