e52217a07c862c2ffd32d19b2cd2022f6d5fb8e5
PedroAraoz <paraoz22@gmail.com>
2026-02-24 10:50:12 -0300

    chore: update eslint config mimic

--- packages/lib-ts/eslint.config.mjs
+++ packages/lib-ts/eslint.config.mjs
@@ -1,11 +1,5 @@
 import eslintConfigMimic from 'eslint-config-mimic'
 
-// Remove react rules
-delete eslintConfigMimic[1].plugins.react
-Object.keys(eslintConfigMimic[1].rules)
-  .filter(rule => rule.includes('react'))
-  .forEach(rule => delete eslintConfigMimic[1].rules[rule])
-
 export default [
   ...eslintConfigMimic,
   {
